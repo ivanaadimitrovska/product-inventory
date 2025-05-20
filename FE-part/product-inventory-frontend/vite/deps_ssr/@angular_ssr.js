b633f18a46a9cfdb23ab0d1bbc54c1cc144367e0
ivanaadimitrovska <ivana.z.dimitrovska@gmail.com>
2025-05-20 13:05:58 +0200

    new folder

---
+++ FE-part/product-inventory-frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-UX2XQZIB.js";
+import "./chunk-G2OASLTA.js";
+import "./chunk-UOMRNSMH.js";
+import "./chunk-73EFIO5B.js";
+import "./chunk-7CWH3INN.js";
+import "./chunk-HGMFHCIT.js";
+import "./chunk-KCIWAPOU.js";
+import "./chunk-CZWOZRQT.js";
+import "./chunk-XOB36AR6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
---
+++ FE-part/product-inventory-frontend/vite/deps_ssr/body-parser.js
@@ -0,0 +1,6 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  require_body_parser
+} from "./chunk-563YCVCO.js";
+import "./chunk-YHCV7DAQ.js";
+export default require_body_parser();
