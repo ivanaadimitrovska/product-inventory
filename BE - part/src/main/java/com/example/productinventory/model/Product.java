b8426d8a88aebf45727fe5579024224e17e2e0c8
ivanaadimitrovska <ivana.z.dimitrovska@gmail.com>
2025-05-20 22:27:23 +0200

    changes

--- BE - part/src/main/java/com/example/productinventory/model/Product.java
+++ BE - part/src/main/java/com/example/productinventory/model/Product.java
@@ -1,6 +1,9 @@
 package com.example.productinventory.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,18 +18,24 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "Name is required")
     @Column(nullable = false)
     private String name;
     
     @Column(length = 1000)
     private String description;
     
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price must be greater than or equal to 0")
     @Column(nullable = false)
     private Double price;
     
+    @NotNull(message = "Quantity in stock is required")
+    @Min(value = 0, message = "Quantity in stock must be greater than or equal to 0")
     @Column(name = "quantity_in_stock", nullable = false)
     private Integer quantityInStock;
     
+    @NotBlank(message = "Category is required")
     @Column(nullable = false)
     private String category;
 
